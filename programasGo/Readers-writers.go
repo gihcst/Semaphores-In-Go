039e35854d536313d15edf576b3916af1e27e0dc
Yasmin Aguirre <yas.cardozo.aguirre@gmail.com>
2024-09-29 22:54:42 -0300

    Refatorando as funções de leitores e escritores, implementando a struct Lightswitch.

--- programasGo/Readers-writers.go
+++ programasGo/Readers-writers.go
@@ -3,51 +3,76 @@ package main
 import (
 	"fmt"
 	"programasGo/FPPDSemaforo"
-	
 )
 
-// variaveis
-var readers int = 0;
-var mutex *FPPDSemaforo.Semaphore = FPPDSemaforo.NewSemaphore(1)
-var roomEmpty *FPPDSemaforo.Semaphore = FPPDSemaforo.NewSemaphore(1)
-
-//Leitores 
-func Readers (f chan struct{}, n int){
-	mutex.Wait()
-	readers+=1
-	if (readers ==1){
-		roomEmpty.Wait()
-	}
-	mutex.Signal()
-	Read(n)
-	mutex.Wait()
-	fmt.Println(n, " Terminou de ler")
-	f <- struct{}{}
-	readers -= 1
-	if (readers == 0){
-		roomEmpty.Signal()
-	}
-	mutex.Signal()
+// Struct para Lightswitch
+type Lightswitch struct {
+	counter int
+	mutex   *FPPDSemaforo.Semaphore
 }
-//Escritores
-func Writers (f chan struct{}, n int ){
+
+// Função para inicializar o Lightswitch
+func NewLightswitch() *Lightswitch {
+	return &Lightswitch{
+		counter: 0,
+		mutex:   FPPDSemaforo.NewSemaphore(1),
+	}
+}
+
+// Função Lock para leitores
+func (ls *Lightswitch) Lock(semaphore *FPPDSemaforo.Semaphore) {
+	ls.mutex.Wait()
+	ls.counter++
+	if ls.counter == 1 {
+		semaphore.Wait()
+	}
+	ls.mutex.Signal()
+}
+
+// Função Unlock para leitores
+func (ls *Lightswitch) Unlock(semaphore *FPPDSemaforo.Semaphore) {
+	ls.mutex.Wait()
+	ls.counter--
+	if ls.counter == 0 {
+		semaphore.Signal()
+	}
+	ls.mutex.Signal()
+}
+
+// Variáveis
+var roomEmpty *FPPDSemaforo.Semaphore = FPPDSemaforo.NewSemaphore(1)
+var lightswitch = NewLightswitch() // Inicializando o lightswitch
+
+// Leitores
+func Readers(f chan struct{}, n int) {
+	// Usando o lightswitch para controlar o acesso dos leitores
+	lightswitch.Lock(roomEmpty)
+	Read(n)
+	fmt.Println(n, " Terminou de ler")
+	lightswitch.Unlock(roomEmpty)
+	f <- struct{}{}
+}
+
+// Escritores
+func Writers(f chan struct{}, n int) {
 	roomEmpty.Wait()
 	Write(n)
 	fmt.Println(n, " Terminou de escrever")
-	roomEmpty.Signal();
+	roomEmpty.Signal()
 	f <- struct{}{}
 }
 
-//Escrever
-func Write (n int){
+// Escrever
+func Write(n int) {
 	fmt.Println(n, " Escrevendo")
 }
-//Ler
-func Read (n int){
+
+// Ler
+func Read(n int) {
 	fmt.Println(n, " Lendo")
 }
 
-func main (){
+func main() {
 	fim := make(chan struct{})
 	for i := 0; i < 10; i++ {
 		go Readers(fim, i)
